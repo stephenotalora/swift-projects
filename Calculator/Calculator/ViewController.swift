41bf2b91c764eb3de605cf8bf5b4d0b18eda17af
Jonathan Otalora <j.otalora@me.com>
2015-09-13 18:49:34 -0700

    initialized public project

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jonathan Otalora  on 2015-09-13.
+//  Copyright (c) 2015 Jonathan Otalora. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # swift-projects
 swift-projects
+A comprehensive compilation of swift applications while deep diving into the details of the Swift laguage.
+
+1. Calculator - Intro to swift.
+
+Jonathan S. Otalora © All rights reserved.
