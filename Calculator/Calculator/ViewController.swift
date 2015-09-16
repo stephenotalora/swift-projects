42d93f992da96dc5e33c2fcd83f83d6baf010b6d
Jonathan Otalora <j.otalora@me.com>
2015-09-16 00:04:56 -0700

    Added perform calculation to view will need to refactor into new object push it to a model

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -11,14 +11,27 @@ import UIKit
 class ViewController: UIViewController {
     // instance variables
     @IBOutlet weak var displayView: UILabel!
-    var userIsInMiddleOfTypingANumber: Bool = false
-
+    var operandStack = Array<Double>()
+    var userIsInMiddleOfTypingANumber = false
+    var displayValue: Double {
+        get{
+            return NSNumberFormatter().numberFromString(displayView.text!)!.doubleValue
+        }
+        set{
+            displayView.text = "\(newValue)"
+            userIsInMiddleOfTypingANumber = false;
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    
+    /**
+     * desc: appends a digit to the end of the string
+     * return: none
+     */
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
         if userIsInMiddleOfTypingANumber {
@@ -30,8 +43,43 @@ class ViewController: UIViewController {
     }
 
     
-    @IBAction func enter(sender: AnyObject) {
-        println(sender.currentTitle)
+    @IBAction func enter() {
+        userIsInMiddleOfTypingANumber = false;
+        operandStack.append(displayValue);
+        println("operandStack = \(operandStack)")
+    }
+    
+    /**
+     * desc: operates on sender
+     * return: none
+     */
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInMiddleOfTypingANumber {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        default: break;
+        }
+    }
+    
+    private func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast());
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
     }
     
     override func didReceiveMemoryWarning() {
