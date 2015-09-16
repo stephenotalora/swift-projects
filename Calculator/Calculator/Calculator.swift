1764c66920af6ef0e46d4e34ae2b399e1d6ac749
Jonathan Otalora <j.otalora@me.com>
2015-09-16 00:09:11 -0700

    added place holder for model refactor

---
+++ Calculator/Calculator/Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  Calculator.swift
+//  Calculator
+//
+//  Created by Jonathan Otalora  on 2015-09-16.
+//  Copyright (c) 2015 Jonathan Otalora. All rights reserved.
+//
+
+import Foundation
+
+class Calculator:NSObject {
+    
+}
